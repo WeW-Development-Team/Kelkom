103ef0149b917f49d20c69f09b110a770a5a2163
CarlesCanos <carlescanos@gmail.com>
2025-11-23 17:41:59 +0100

    First components and configs

--- Kelkom-front/src/app/app.routes.ts
+++ Kelkom-front/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './components/public/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingPage },
+    {path: '**', component: LandingPage } //Allways last
+];
--- Kelkom-front/src/app/app.ts
+++ Kelkom-front/src/app/app.ts
@@ -4,9 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('KelkomGames');
+  protected readonly title = signal('Kelkom  Games');
 }
